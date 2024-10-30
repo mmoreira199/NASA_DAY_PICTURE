c87e2b7367be7049bc5df1ede0f1a60c5154da64
mmoreira199 <126065356+mmoreira199@users.noreply.github.com>
2024-10-30 09:31:19 +0100

    Update ImagedujourNASA_Streamlit.py

--- ImagedujourNASA_Streamlit.py
+++ ImagedujourNASA_Streamlit.py
@@ -16,30 +16,23 @@ import requests
 from io import BytesIO
 from PIL import Image, ImageDraw
 
-NASA_API_KEY = 'vBsbRn3IvW8j1Mnn3n4Aj3ReLJs9b1AKh37rQa1N'
-adresse = f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}'
-
-
-
-def getimage(adresse):
-    
-    adresse=adresse+'&date='+datechoisie
+def getimage():
+    adresse = f'https://api.nasa.gov/planetary/apod?api_key={st.secrets["nasa"]["api_key"]}'
+    adresse = adresse + '&date=' + datechoisie
     response = requests.get(adresse)
-    data=response.json()
-    url=data['url']
-    legende=data['explanation']
+    data = response.json()
+    url = data['url']
+    legende = data['explanation']
     res = requests.get(url)
-    image=res.content
+    image = res.content
     image = Image.open(BytesIO(image))
-    return image,legende
-     
-st.title("The astronomy picture of the Day provided by NASA")
-st.header("using API")
+    return image, legende
 
+st.title("The astronomy picture of the Day provided by NASA")
 datechoisie = st.date_input("Date ?")
-datechoisie=str(datechoisie)
+datechoisie = str(datechoisie)
 st.write("date choisie:", datechoisie)
-image,legende=getimage(adresse)
+image, legende = getimage()
 st.image(image)
 st.caption(legende)
 
